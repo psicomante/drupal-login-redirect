19a4f303aa2221916ff2e9e184c266d825b8b927
Roberto Pesando <roberto.pesando@gmail.com>
2015-11-08 08:14:42 +0100

    update the module, so it's working on latest RC

--- src/Form/LoginRedirectAdminForm.php
+++ src/Form/LoginRedirectAdminForm.php
@@ -8,6 +8,7 @@
 namespace Drupal\login_redirect\Form;
 
 use Drupal\Core\Form\ConfigFormBase;
+use Drupal\Core\Form\FormStateInterface;
 
 /**
  * Configure Login Redirect settings for this site.
@@ -25,39 +26,55 @@ class LoginRedirectAdminForm extends ConfigFormBase {
   /**
    * {@inheritdoc}
    */
-  public function buildForm(array $form, array &$form_state) {
-    $config = $this->config('login_redirect.parameters');
+  public function buildForm(array $form, FormStateInterface $form_state) {
+
+    // Default settings
+    $config = $this->config('login_redirect.settings');
 
     $form['status'] = array(
       '#type' => 'radios',
-      '#options' => array(0 => t('Disabled'), 1 => t('Enabled')),
-      '#title' => t('Module status'),
-      '#default_value' => $config->get('status'),
-      '#description' => t('Should the module be enabled?'),
+      '#options' => array(0 => $this->t('Disabled'), 1 => $this->t('Enabled')),
+      '#title' => $this->t('Module status'),
+      '#default_value' => $config->get('login_redirect.status'),
+      '#description' => $this->t('Should the module be enabled?'),
     );
 
-    $form['parameter_name'] = array(
+    $form['destination'] = array(
       '#type' => 'textfield',
-      '#title' => t('Parameter Name'),
-      '#default_value' => $config->get('parameter_name'),
+      '#title' => $this->t('Destination'),
+      '#default_value' => $config->get('login_redirect.destination'),
       '#maxlength' => 255,
-      '#description' => t('Enter user defined query parameter name same as we have q in drupal core. For example if the parameter name is set to "destination", then you would visit user/login&destination=(redirect destination).'),
+      '#description' => $this->t('Enter user defined query parameter name same as we have q in drupal core. For example if the parameter name is set to "destination", then you would visit user/login&destination=(redirect destination).'),
     );
 
     return parent::buildForm($form, $form_state);
   }
 
   /**
-   * {@inheritdoc}
+   * {@inheritdoc}.
    */
-  public function submitForm(array &$form, array &$form_state) {
-    $config = $this->config('login_redirect.parameters')
-      ->set('status', $form_state['values']['status'])
-      ->set('parameter_name', $form_state['values']['parameter_name']);
+  public function validateForm(array &$form, FormStateInterface $form_state) {
 
+  }
+
+  /**
+   * {@inheritdoc}.
+   */
+  public function submitForm(array &$form, FormStateInterface $form_state) {
+    $config = $this->config('login_redirect.settings');
+    $config->set('login_redirect.status', $form_state->getValue('status'));
+    $config->set('login_redirect.destination', $form_state->getValue('destination'));
     $config->save();
+    return parent::submitForm($form, $form_state);
+  }
 
-    parent::submitForm($form, $form_state);
+  /**
+   * {@inheritdoc}.
+   */
+  protected function getEditableConfigNames() {
+    return [
+      'login_redirect.settings',
+    ];
   }
 
 }
